a2a19629eff49ad1474f285216af3a2a0fbf4054
Achintya8 <achintyak30@gmail.com>
2025-08-11 09:49:18 +0000

    Code challenge submission for question: 31 Next Permutation

---
+++ 31 Next Permutation/31next-permutation.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+     int n = nums.length;
+        
+        // Step 1: Find pivot
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        
+        if (i >= 0) { // If not the last permutation
+            // Step 2: Find successor
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            // Step 3: Swap
+            swap(nums, i, j);
+        }
+        
+        // Step 4: Reverse suffix
+        reverse(nums, i + 1, n - 1);
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;}   
+    }
+}
