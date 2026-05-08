0126ba620502be13f6d28483af59323746d278a3
Achintya8 <achintyak30@gmail.com>
2026-05-08 11:02:21 +0000

    Code challenge submission for question: 326 Power of Three

---
+++ 326 Power of Three/326power-of-three.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        // Base cases
+        if (n <= 0) return false;
+        if (n == 1) return true; // 3^0 = 1
+        
+        // If n is divisible by 3, keep dividing
+        if (n % 3 == 0) {
+            return isPowerOfThree(n / 3);
+        }
+        
+        return false;
+    }
+}
