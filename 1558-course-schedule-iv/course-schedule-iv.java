54108ed9f39d7f7bab80abffcfddca70d30e7d8d
Achintya K <103931945+Achintya8@users.noreply.github.com>
2026-06-15 22:31:04 +0530

    Time: 12 ms (92.79%) | Memory: 50.4 MB (76.89%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.java
+++ 1558-course-schedule-iv/course-schedule-iv.java
@@ -10,15 +10,15 @@ class Solution {
             adj[i] = new ArrayList<>();
         }
         
-        // Build graph and initialize direct prerequisites
+        
         for (int[] edge : prerequisites) {
             int a = edge[0], b = edge[1];
             adj[a].add(b);
-            prereq[b][a] = true;  // Direct prerequisite from a to b
+            prereq[b][a] = true;  
             inDegree[b]++;
         }
         
-        // Topological sort using Kahn's algorithm
+       
         Queue<Integer> q = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
             if (inDegree[i] == 0) q.add(i);
@@ -27,7 +27,7 @@ class Solution {
         while (!q.isEmpty()) {
             int u = q.poll();
             for (int v : adj[u]) {
-                // Merge all prerequisites of u into v
+                
                 for (int i = 0; i < numCourses; i++) {
                     if (prereq[u][i]) prereq[v][i] = true;
                 }
@@ -35,7 +35,7 @@ class Solution {
             }
         }
         
-        // Answer queries using precomputed prerequisites
+       
         List<Boolean> ans = new ArrayList<>();
         for (int[] query : queries) {
             int u = query[0], v = query[1];
